b8eadd718fe7d6512a944633a7a3bd9a02d5e349
Adin Piric <adinpiric18@gmail.com>
2026-05-17 13:11:03 +0200

    dodan splash screen i login diazajn

---
+++ app/src/main/java/com/example/humanitasconnect/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.humanitasconnect;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+        TextView tvGoToRegister = findViewById(R.id.tvGoToRegister);
+
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+        tvGoToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
